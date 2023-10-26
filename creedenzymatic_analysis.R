a3dda8a6a8b3ca84749416cf1a56f29cc95875d5
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2023-10-26 11:28:38 -0400

    fix: update creedenzymatic analysis script

--- creedenzymatic_analysis.R
+++ creedenzymatic_analysis.R
@@ -1,20 +1,23 @@
 # Creedenzymatic Analysis
 
 library(tidyverse)
+library(dplyr)
+library(readr)
+library(stringr)
 library(creedenzymatic)
 
 process_creedenzymatic <-
   function(krsa_path, uka_path, peptide_path) {
-    krsa_data <- read_csv(krsa_path, show_col_types = FALSE) |>
+    krsa_data <- readr::read_csv(krsa_path, show_col_types = FALSE) |>
       select(Kinase, Score = AvgZ) |>
       read_krsa(trns = "abs", sort = "desc")
 
-    uka_data <- read_tsv(uka_path, show_col_types = FALSE) |>
-      select(Kinase = `Kinase Name`, Score = `Median Final score`) |>
+    uka_data <- readr::read_tsv(uka_path, show_col_types = FALSE) |>
+      dplyr::select(Kinase = `Kinase Name`, Score = `Median Final score`) |>
       read_uka(trns = "abs", sort = "desc")
 
     peptide_data <-
-      read_csv(peptide_path, show_col_types = FALSE) |>
+      readr::read_csv(peptide_path, show_col_types = FALSE) |>
       select(Peptide, Score = totalMeanLFC)
 
     kea3_data <-
@@ -39,17 +42,20 @@ process_creedenzymatic <-
     combined
   }
 
-krsa_files <- list.files("results", "krsa", full.names = TRUE) |> keep(~ str_detect(.x, "Pat27|Pat31")) |> sort()
+krsa_files <- list.files("results", "krsa", full.names = TRUE) |>
+  keep(~ str_detect(.x, "Pat27|Pat31")) |>
+  sort()
 
 uka_files <- c(
   "kinome_data/PTK/UKA/Pat27/Summaryresults 20231025-1512.txt",
   "kinome_data/STK/UKA/Pat27/Summaryresults 20231024-1346.txt",
   "kinome_data/PTK/UKA/Pat31/Summaryresults 20231025-1527.txt",
   "kinome_data/STK/UKA/Pat31/Summaryresults 20231024-1356.txt"
-
 )
 
-peptide_files <- list.files("results", "dpp", full.names = TRUE) |> keep(~ str_detect(.x, "Pat27|Pat31")) |> sort()
+peptide_files <- list.files("results", "dpp", full.names = TRUE) |>
+  keep(~ str_detect(.x, "Pat27|Pat31")) |>
+  sort()
 
 result_names <- str_extract(krsa_files, "full_(.*)\\.csv", 1)
 
@@ -61,4 +67,7 @@ result <-
   ) |>
   pmap(process_creedenzymatic) |>
   set_names(result_names) |>
-  imap_dfr(~ write_csv(.x, str_glue("results/{.y}_creedenzymatic.csv")), .id = "Comparison")
+  imap_dfr(~ write_csv(
+    .x,
+    str_glue("results/{.y}_creedenzymatic.csv")
+  ), .id = "Comparison")
